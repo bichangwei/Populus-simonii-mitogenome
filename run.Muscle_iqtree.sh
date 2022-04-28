d6b1a5d1a54b6ab420a52b57bf992aeef1fba100
bichangwei <bichwei@163.com>
2022-04-28 19:19:14 +0800

    update

--- run.Muscle_iqtree.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#########################################################################
-# FileName: run.Muscle_iqtree.sh
-# Version: dc211e42-373c-43f3-9970-520d098bd1bc
-# Author: Changwei <bichwei@163.cn>
-# CreatedTime: Thu Sep  9 09:24:22 2021
-#########################################################################
-muscle -in Populus_simonii.phylogeny.fa -out Populus_simonii.muscle.alignment.afa -maxiters 2
-iqtree -s Populus_simonii.muscle.alignment.afa -m MFP -B 1000 --bnni -T AUTO
